14cf69157c13b7c4ec0ca370f783bb6419467b91
agustinbrusco <agustin.brusco@gmail.com>
2023-09-28 16:20:54 -0300

    moved retry measurements and recovered old version

--- skipper_data/scripts_minilambda/loop_background_retry.sh
+++ skipper_data/scripts_minilambda/loop_background_retry.sh
@@ -2,7 +2,7 @@
 
 Vvsub=70  # V: Tensión de sustrato a 70 V post limpiar
 
-imgFOLDER=`dirname $BASH_SOURCE`/images/TRAPS/29SEP2023
+imgFOLDER=`dirname $BASH_SOURCE`/images/TRAPS/28SEP2023
 runname=no_cover_shutter_calibration
 
 lockfilename=lockfile  # kind of an Env Var
@@ -21,7 +21,7 @@ doSettings(){
 	lta NROW $rows
     	cols=336
 	lta NCOL $cols
-    	nsmpls=25
+    	nsmpls=1
 	lta NSAMP $nsmpls
     	expo=0
 	lta EXPOSURE $expo
@@ -40,12 +40,17 @@ lta set vsub $Vvsub
 doClean
 doClean
 doClean
-doClean
+# doClean  # fourth doClean inside loop
 
 for i in 1 2 3 4 5 6 7 8 9 10
 do
     for exposure_time in 300 600 900 1200 1500 1800 # seconds
     do
+		# the while loop repeats the measurement until it's succesful
+		# a measurement is succesful if no .dat symbolic links have been created at the end of the loop
+		succes_read=false
+		while [ $succes_read = false ]
+		do
             if [ ! -f "$lockfilename" ]; then break; fi
 
             doClean
@@ -54,5 +59,11 @@ do
             doSettings
             lta name $imgFOLDER/skp_${runname}_NSAMP${nsmpls}_NROW${rows}_NCOL${cols}_EXPOSURE${exposure_time}_img
             lta read
+			if [[ $(ls $imgFOLDER | grep -c .dat$) -eq 0 ]]; then
+				succes_read=true
+			else
+				rm $imgFOLDER/*.dat
+			fi
+		done
     done
 done
